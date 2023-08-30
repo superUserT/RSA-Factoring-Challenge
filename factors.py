05dafa250926921f251c5dd01c8c7238e315fa4e
superUserT <rantshothabisomail@gmail.com>
2023-08-30 15:09:37 +0200

    new file

---
+++ factors.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+
+
+"""Represents a factorising fuction"""
+def f_factorize_func(n):
+    for f in range(2, n // 2 + 1):
+        if n % f == 0:
+            return f, n // f
+    return n, 1
+
+
+"""Represent the main entry for the function"""
+def main():
+    if len(sys.argv) != 2:
+        print("Operation Failure")
+        return
+
+    input_file = sys.argv[1]
+
+    try:
+        with open(input_file, 'r') as file:
+            numbers = file.read().splitlines()
+
+            for num in numbers:
+                n = int(num)
+                p, q = f_factorize_func(n)
+                print(f"{n}={p}*{q}")
+
+    except FileNotFoundError:
+        print(f"File '{input_file}' not found.")
+    except ValueError:
+        print("Invalid number in the file.")
+
+
+"""Represents the exectution of the function main"""
+if __name__ == "__main__":
+    main()
